4d3ad3a2b1be9a6d8d6d120f2dec41f4508d91d4
tkmynrg <dlsenok@mail.ru>
2023-07-05 23:34:39 +0300

    feat: implement CV

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ navBurger.addEventListener('click', () => {
         body.classList.add('mobile-menu-open');
     }
 });
+
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+
+        const targetId = link.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        const scrollTo = targetElement.offsetTop - 80;
+
+        window.scrollTo({
+            top: scrollTo,
+            behavior: 'smooth'
+        });
+
+        document.body.classList.remove('mobile-menu-open');
+    });
+});
